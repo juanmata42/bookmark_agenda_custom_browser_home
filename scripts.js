84af217532391b1c954c19d9fdd91cfc381a9cf0
juanma42 <juanmanueltabalv@gmail.com>
2024-12-08 16:13:10 +0100

    almost

--- scripts.js
+++ scripts.js
@@ -46,16 +46,109 @@ const links = [
     { "text": "Notion", "url": "https://www.notion.so/", "iconUrl": "icons/notion.png" },
     { "text": "AWS", "url": "https://aws.amazon.com/es/?nc2=h_lg", "iconUrl": "icons/aws.png" },
 ];
+// Using https://www.flaticon.com/ for icons on this case
 const linkPages = [
-    {title: "Favoritos", icon: "icons/favorites.png", color: red, links:[
-        { "text": "ChatGPT", "url": "https://chatgpt.com/", "iconUrl": "icons/chatgpt.png" },
-        { "text": "Figma", "url": "https://www.figma.com", "iconUrl": "icons/figma.png" },
-        { "text": "GitHub", "url": "https://github.com/juanmata42", "iconUrl": "icons/github.png" },
-        { "text": "Calendar", "url": "https://calendar.google.com/calendar/u/0/r", "iconUrl": "icons/calendar.png" },
-        { "text": "Google Keep", "url": "https://keep.google.com/#home", "iconUrl": "icons/keep.png" },
-    ]},
+    // 1. Favorites: Easy access to frequently used links
+    {
+        title: "Favoritos",
+        icon: "icons/favorites.svg",
+        color: colors.red,
+        links: [
+            { "text": "ChatGPT", "url": "https://chatgpt.com/", "iconUrl": "icons/chatgpt.png" },
+            { "text": "Figma", "url": "https://www.figma.com", "iconUrl": "icons/figma.png" },
+            { "text": "GitHub", "url": "https://github.com/juanmata42", "iconUrl": "icons/github.png" },
+            { "text": "Calendar", "url": "https://calendar.google.com/calendar/u/0/r", "iconUrl": "icons/calendar.png" },
+            { "text": "Google Keep", "url": "https://keep.google.com/#home", "iconUrl": "icons/keep.png" },
+            { "text": "Calendar", "url": "https://calendar.google.com/calendar/u/0/r", "iconUrl": "icons/calendar.png" },
+            { "text": "ChatGPT", "url": "https://chatgpt.com/", "iconUrl": "icons/chatgpt.png" },
+            { "text": "Confluence", "url": "https://www.atlassian.com/es/software/confluence", "iconUrl": "icons/confluence.png" },
+            { "text": "Google Drive", "url": "https://drive.google.com/drive/shared-with-me", "iconUrl": "icons/drive.png" },
+            { "text": "Factorial", "url": "https://app.factorialhr.com/dashboard", "iconUrl": "icons/factorial.png" },
+            { "text": "Figma", "url": "https://www.figma.com", "iconUrl": "icons/figma.png" },
+            { "text": "GitHub", "url": "https://github.com/juanmata42", "iconUrl": "icons/github.png" },
+            { "text": "Gmail", "url": "https://mail.google.com/mail/u/0/#inbox", "iconUrl": "icons/gmail.png" },
+            { "text": "Jira", "url": "https://www.atlassian.com/es/software/jira", "iconUrl": "icons/jira.png" },
+            { "text": "Google Keep", "url": "https://keep.google.com/#home", "iconUrl": "icons/keep.png" },
+            { "text": "Miro", "url": "https://miro.com/app/dashboard/", "iconUrl": "icons/miro.png" },
+            { "text": "Notion", "url": "https://www.notion.so/", "iconUrl": "icons/notion.png" },
+            { "text": "AWS", "url": "https://aws.amazon.com/es/?nc2=h_lg", "iconUrl": "icons/aws.png" },
+            { "text": "Calendar", "url": "https://calendar.google.com/calendar/u/0/r", "iconUrl": "icons/calendar.png" },
+            { "text": "ChatGPT", "url": "https://chatgpt.com/", "iconUrl": "icons/chatgpt.png" },
+            { "text": "Confluence", "url": "https://www.atlassian.com/es/software/confluence", "iconUrl": "icons/confluence.png" },
+            { "text": "Google Drive", "url": "https://drive.google.com/drive/shared-with-me", "iconUrl": "icons/drive.png" },
+            { "text": "Factorial", "url": "https://app.factorialhr.com/dashboard", "iconUrl": "icons/factorial.png" },
+            { "text": "Figma", "url": "https://www.figma.com", "iconUrl": "icons/figma.png" },
+            { "text": "GitHub", "url": "https://github.com/juanmata42", "iconUrl": "icons/github.png" },
+            { "text": "Gmail", "url": "https://mail.google.com/mail/u/0/#inbox", "iconUrl": "icons/gmail.png" },
+            { "text": "Jira", "url": "https://www.atlassian.com/es/software/jira", "iconUrl": "icons/jira.png" },
+            { "text": "Google Keep", "url": "https://keep.google.com/#home", "iconUrl": "icons/keep.png" },
+            { "text": "Miro", "url": "https://miro.com/app/dashboard/", "iconUrl": "icons/miro.png" },
+            { "text": "Notion", "url": "https://www.notion.so/", "iconUrl": "icons/notion.png" },
+            { "text": "AWS", "url": "https://aws.amazon.com/es/?nc2=h_lg", "iconUrl": "icons/aws.png" },
+            { "text": "Calendar", "url": "https://calendar.google.com/calendar/u/0/r", "iconUrl": "icons/calendar.png" },
+            { "text": "ChatGPT", "url": "https://chatgpt.com/", "iconUrl": "icons/chatgpt.png" },
+            { "text": "Confluence", "url": "https://www.atlassian.com/es/software/confluence", "iconUrl": "icons/confluence.png" },
+            { "text": "Google Drive", "url": "https://drive.google.com/drive/shared-with-me", "iconUrl": "icons/drive.png" },
+            { "text": "Factorial", "url": "https://app.factorialhr.com/dashboard", "iconUrl": "icons/factorial.png" },
+            { "text": "Figma", "url": "https://www.figma.com", "iconUrl": "icons/figma.png" },
+        ]
+    },
+    // 2. Productivity: Tools for organizing and managing tasks
+    {
+        title: "Productividad",
+        icon: "icons/productivity.svg",
+        color: colors.green,
+        links: [
+            { "text": "Calendar", "url": "https://calendar.google.com/calendar/u/0/r", "iconUrl": "icons/calendar.png" },
+            { "text": "Google Keep", "url": "https://keep.google.com/#home", "iconUrl": "icons/keep.png" },
+            { "text": "Notion", "url": "https://www.notion.so/", "iconUrl": "icons/notion.png" },
+            { "text": "Miro", "url": "https://miro.com/app/dashboard/", "iconUrl": "icons/miro.png" },
+        ]
+    },
+    // 3. Work: Links related to professional tasks and tools
+    {
+        title: "Documentación",
+        icon: "icons/documentation.svg",
+        color: colors.blue,
+        links: [
+            { "text": "Jira", "url": "https://www.atlassian.com/es/software/jira", "iconUrl": "icons/jira.png" },
+            { "text": "Confluence", "url": "https://www.atlassian.com/es/software/confluence", "iconUrl": "icons/confluence.png" },
+            { "text": "Factorial", "url": "https://app.factorialhr.com/dashboard", "iconUrl": "icons/factorial.png" },
+            { "text": "AWS", "url": "https://aws.amazon.com/es/?nc2=h_lg", "iconUrl": "icons/aws.png" },
+        ]
+    },
+    // 4. Design: Tools for visual work and design collaboration
+    {
+        title: "Diseño",
+        icon: "icons/design.svg",
+        color: colors.orange,
+        links: [
+            { "text": "Figma", "url": "https://www.figma.com", "iconUrl": "icons/figma.png" },
+            { "text": "Miro", "url": "https://miro.com/app/dashboard/", "iconUrl": "icons/miro.png" },
+        ]
+    },
+    // 5. Development: Tools for coding, repositories, and AI
+    {
+        title: "Desarrollo",
+        icon: "icons/development.svg",
+        color: colors.purple,
+        links: [
+            { "text": "GitHub", "url": "https://github.com/juanmata42", "iconUrl": "icons/github.png" },
+            { "text": "ChatGPT", "url": "https://chatgpt.com/", "iconUrl": "icons/chatgpt.png" },
+            { "text": "AWS", "url": "https://aws.amazon.com/es/?nc2=h_lg", "iconUrl": "icons/aws.png" },
+        ]
+    },
+    // 6. Communication: Tools for email and messaging
+    {
+        title: "Comunicación",
+        icon: "icons/communication.svg",
+        color: colors.yellow,
+        links: [
+            { "text": "Gmail", "url": "https://mail.google.com/mail/u/0/#inbox", "iconUrl": "icons/gmail.png" },
+        ]
+    },
+];
 
-]
 // Array of image paths. Add more images to the array to display them randomly
 // Don't just use the biggest res possible, since loading times will be longer. 
 // In my case, a 1000x750 px jpg image is more than enough, i can't appreciate the pixels on my 1920x1080 screen. it weights 329kb
@@ -77,7 +170,6 @@ document.addEventListener("DOMContentLoaded", function () {
     for (const color in colors) {
         // Create a new div element
         const colorBox = document.createElement('div');
-
         // Add classes to the div
         colorBox.className = `color-box ${color}`;
 
@@ -120,11 +212,88 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Get links container
     const linksContainer = document.getElementById('links-container');
+    // Iterate through linkPages and create the UI elements
+    linkPages.forEach((page, index) => {
+        // Create the linksPage container
+        const linksPage = document.createElement('div');
+        linksPage.className = `links-page page-${index}`; // Add a class for styling
+        linksPage.id = `page-${index}`; // Assign an ID based on the index
+        // Create the heade element that contains the title and the icon
+        const linksPageHeader = document.createElement('div');
+        linksPageHeader.className = 'links-page-header'; // Add a class for styling
+        linksPageHeader.style.borderColor = page.color; // Set the border color
 
+        // Create the page interior container
+        const pageInteriorContainer = document.createElement('div');
+        pageInteriorContainer.className = 'page-interior-container'
+        pageInteriorContainer.style.borderColor = page.color; // Set the border color
+        // Create the title element
+        const linksPageTitle = document.createElement('h1');
+        linksPageTitle.className = 'links-page-title'; // Add a class for styling
+        linksPageTitle.textContent = page.title; // Set the title text
+
+        // Create the icon element
+        const linksPageIcon = document.createElement('img');
+        linksPageIcon.className = 'links-page-icon'; // Add a class for styling
+        linksPageIcon.src = page.icon; // Set the icon source
+        linksPageIcon.alt = `${page.title} Icon`; // Add alt text
+
+        // Append the title and icon to the header
+        linksPageHeader.appendChild(linksPageIcon);
+        linksPageHeader.appendChild(linksPageTitle);
+        // Append the Header to the interior container
+        pageInteriorContainer.appendChild(linksPageHeader);
+
+        // Create the indexTab container
+        const indexTab = document.createElement('div');
+        indexTab.className = `index-tab page-${index}`; // Add a class for styling
+        indexTab.id = `index-tab-${index}`; // Assign an ID based on the index
+        indexTab.style.backgroundColor = page.color; // Set the background color
+        // Create another icon inside the indexTab
+        const indexTabIcon = document.createElement('img');
+        indexTabIcon.className = 'index-tab-icon'; // Add a class for styling
+        indexTabIcon.src = page.icon; // Set the icon source
+        indexTabIcon.alt = `${page.title} Icon`; // Add alt text
+        // Append the icon to the indexTab
+        indexTab.appendChild(indexTabIcon);
+        const linksGrid = document.createElement('div')
+        linksGrid.className = 'links-grid';
+        // Add individual links to the indexTab
+        page.links.forEach(link => {
+            const linkItem = document.createElement('a');
+            linkItem.className = 'link-item'; // Add a class for styling
+            linkItem.href = link.url; // Set the link URL
+            linkItem.target = '_blank'; // Open links in a new tab
+
+            const linkIcon = document.createElement('img');
+            linkIcon.className = 'link-icon'; // Add a class for styling
+            linkIcon.src = link.iconUrl; // Set the link's icon
+            linkIcon.alt = `${link.text} Icon`;
+
+            const linkText = document.createElement('span');
+            linkText.className = 'link-text'; // Add a class for styling
+            linkText.textContent = link.text;
+
+            // Append icon and text to the link
+            linkItem.appendChild(linkIcon);
+            linkItem.appendChild(linkText);
+
+            // Append the link to the indexTab
+            linksGrid.appendChild(linkItem);
+        });
+        pageInteriorContainer.appendChild(linksGrid)
+        // Append links to Page interior container
+        linksContainer.appendChild(linksPage);
+        // Append title, icon, and indexTab to the linksPage
+        linksPage.appendChild(pageInteriorContainer);
+        linksPage.appendChild(indexTab);
+
+        // Append the linksPage to the linksContainer
+    });
     // Iterate over the links array to create anchor elements
     /* links.forEach(link => {
         const a = document.createElement('a');
-        a.href = link.url;  // Set the href attribute
+        a.href = link.url;  // Set tzzhe href attribute
         a.target = '_self';  // Make the link open in the same tab
         a.className = 'link-item';  // Add a class for styling
 
@@ -164,5 +333,5 @@ document.addEventListener("DOMContentLoaded", function () {
 
 } */
 // Run the function on page load and page resize
-window.addEventListener('load', setLinksContainerHeight);
-window.addEventListener('resize', setLinksContainerHeight);
+/* window.addEventListener('load', setLinksContainerHeight);
+window.addEventListener('resize', setLinksContainerHeight); */
